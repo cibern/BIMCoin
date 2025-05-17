31dfacacd71159f49a0ff740c04469a488e85717
serveisprototip <cibern@gmail.com>
2025-05-17 07:57:54 +0200

    Primera versió del projecte BIMCoin

---
+++ src/Stats.js
@@ -0,0 +1,7 @@
+const stats = new Stats();
+stats.showPanel(2);
+document.body.append(stats.dom);
+stats.dom.style.left = "0px";
+stats.dom.style.zIndex = "unset";
+world.renderer.onBeforeUpdate.add(() => stats.begin());
+world.renderer.onAfterUpdate.add(() => stats.end());
