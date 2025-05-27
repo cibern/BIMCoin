fc666eace473e1e1bef0f591ce130c7a847d39fc
serveisprototip <cibern@gmail.com>
2025-05-27 09:59:38 +0200

    Prova 11

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/BIMCoin/', // Correcte per a GitHub Pages en el repositori BIMCoin
+  base: '/BIMCoin/',
   build: {
-    outDir: 'docs', // On es generen els fitxers
+    outDir: 'docs',
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
@@ -13,10 +12,16 @@ export default defineConfig({
         verify: resolve(__dirname, 'verify.html'),
         porpuse: resolve(__dirname, 'porpuse.html'),
         faqs: resolve(__dirname, 'faqs.html'),
+      },
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash][extname]',
       }
     }
   }
 });
+
 
 
 
